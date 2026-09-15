2093ae9f2d42b84409d259fc046c8f6e8c419e62
Morpheos <morphylee22@gmail.com>
2026-09-15 06:09:50 +0100

    fix: keep Athena private and clean up pricing page

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,16 +1,9 @@
 "use client";
 
-import { AthenaWidget } from "@/components/athena/athena-widget";
-
 export default function PublicLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <>
-      {children}
-      <AthenaWidget />
-    </>
-  );
+  return <>{children}</>;
 }
